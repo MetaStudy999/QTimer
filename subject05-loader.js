f63f8278f03a676c0d7b906b738c6fee8c7b4e6b
YOUNG SE PARK <110063933+MetaStudy999@users.noreply.github.com>
2026-08-11 08:24:33 +0900

    feat: load subject 5 chapter 1 complete and chapter 2 start

--- subject05-loader.js
+++ subject05-loader.js
@@ -3,3 +3,5 @@ document.write('<script src="./subject05-ch01-01-06.js"><\/script>');
 document.write('<script src="./subject05-ch01-07-20.js"><\/script>');
 document.write('<script src="./subject05-ch01-21-43.js"><\/script>');
 document.write('<script src="./subject05-ch01-44-55.js"><\/script>');
+document.write('<script src="./subject05-ch01-56-58.js"><\/script>');
+document.write('<script src="./subject05-ch02-01-03.js"><\/script>');
