120724f4615ce928e23be36cfb3aa27421005346
YOUNG SE PARK <110063933+MetaStudy999@users.noreply.github.com>
2026-08-11 08:18:55 +0900

    feat: add subject 5 question loader

---
+++ subject05-loader.js
@@ -0,0 +1,4 @@
+// Subject 5 loader: keep index.html stable while rapidly adding question batches.
+document.write('<script src="./subject05-ch01-01-06.js"><\/script>');
+document.write('<script src="./subject05-ch01-07-20.js"><\/script>');
+document.write('<script src="./subject05-ch01-21-43.js"><\/script>');
